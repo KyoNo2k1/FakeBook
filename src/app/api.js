ed2483a67e51edff5c028a2794d3e2d0b5252019
Lê Trung Nghĩa <90697464+KyoNo2k1@users.noreply.github.com>
2022-02-12 16:07:25 +0700

    change link backend

--- src/app/api.js
+++ src/app/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
-const API = axios.create({ baseURL: 'http://localhost:5000/'})
-// const API = axios.create({ baseURL: 'https://dashboard.heroku.com/apps/fakebookbackenddeploy/'})
+// const API = axios.create({ baseURL: 'http://localhost:5000/'})
+const API = axios.create({ baseURL: 'https://fakebookbackenddeploy.herokuapp.com/'})
 
 API.interceptors.request.use((req) => {
     if(localStorage.getItem('profile')){
