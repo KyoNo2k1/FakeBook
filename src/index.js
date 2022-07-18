e7d4013a72b59ccc0f1669ef05a23801aa37354e
Lê Trung Nghĩa <nghia567123@gmail.com>
2022-07-18 16:25:13 +0700

    bug mui v5 downgrade react 17

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import App from "./App";
 import "./index.css";
 import store from "./components/redux/store";
 import StyledEngineProvider from "@mui/material/StyledEngineProvider";
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+
+ReactDOM.render(
   <React.Fragment>
     <StyledEngineProvider injectFirst>
       <Provider store={store}>
         <App />
       </Provider>
     </StyledEngineProvider>
-  </React.Fragment>
+  </React.Fragment>,
+  document.getElementById("root")
 );
